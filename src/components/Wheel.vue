22284d3e1625a6435a115fa4b72b2f5ed7e84049
emingenc <emingenc@novit.ai>
2021-08-01 00:29:24 +0300

    ADD: wheel added

---
+++ src/components/Wheel.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="wrap" @click="$refs.wheel.spin">
+    <Wheel
+      ref="wheel"
+      :gift="gift"
+      :data="data"
+      @done="done"
+      :imgParams="logo"
+    />
+  </div>
+</template>
+
+<script >
+import { defineComponent } from "vue";
+import { Wheel } from "vue3-fortune-wheel";
+export default defineComponent({
+  name: "App",
+  components: {
+    Wheel,
+  },
+  data() {
+    return {
+      gift: 5,
+      data: [
+        {
+          id: 1,
+          value: "Gift 1",
+          bgColor: "#7d7db3",
+          color: "#ffffff",
+        },
+        {
+          id: 2,
+          value: "Gift 2",
+          bgColor: "#ffffff",
+          color: "#000000",
+        },
+        {
+          id: 3,
+          value: "Gift 3",
+          bgColor: "#c92729",
+          color: "#ffffff",
+        },
+        {
+          id: 4,
+          value: "Gift 4",
+          bgColor: "#7d7db3",
+          color: "#ffffff",
+        },
+        {
+          id: 5,
+          value: "Gift 5",
+          bgColor: "#ffffff",
+          color: "#000000",
+        },
+        {
+          id: 6,
+          value: "Gift 6",
+          bgColor: "#c92729",
+          color: "#ffffff",
+        },
+      ],
+      logo: {
+        src: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png",
+        width: 100,
+        height: 120,
+      },
+    };
+  },
+  methods: {
+    done(r) {
+      console.log(r);
+    },
+  },
+});
+</script>
+
+<style>
+.wrap {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,20 @@
 <template>
   <q-page class="flex bg-red-5 flex-center">
     <Timer/>
+    <Wheel/>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 import Timer from 'components/Timer.vue';
+import Wheel from 'components/Wheel.vue';
 
 export default defineComponent({
   name: 'PageIndex',
   components: {
     Timer,
+    Wheel,
   },
 })
 </script>
