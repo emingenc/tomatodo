5a67c62819a3c8c5514550727286ddeb29d61e73
emingenc <emingenc@novit.ai>
2021-08-14 22:12:02 +0300

    FIX: width fixed

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,7 @@
 <template>
-  <q-page class="flex bg-red-3 flex-center">
-    <div @click="this.store.state.step ++" class="q-pa-md">
-      <q-list style="width: 100%" class="justify-around">
+  <q-page @dblclick="this.store.state.step --" class="flex bg-red-3 flex-center">
+    <div @dblclick="this.store.state.step ++" class="q-pa-md">
+      <q-list style="width: 750px" class="justify-around">
         <q-item
          
           tag="label"
