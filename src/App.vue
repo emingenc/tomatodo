5f95b5f0b2956e2ccf617a989d12f29d03a5eba3
emingenc <emingenc@novit.ai>
2021-08-22 23:47:39 +0300

    ADD: local store

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,25 @@
 </template>
 <script>
 import { defineComponent, provide } from 'vue';
+import { LocalStorage } from 'quasar'
 import store from './store'
 
 export default defineComponent({
   name: 'App',
   setup(){
     
+    const lstore = LocalStorage.getItem('localStore')
     
-    window.addEventListener("beforeunload", ()=>{console.log('')})
+   
+
+    window.addEventListener("beforeunload", ()=>{$q.localStorage.set('localStore', store.state)})
     window.onbeforeunload = function(){
     return "Are you sure you want to close the window?";
 }
     provide('store',store)
+  },
+  mounted(){
+    this.store.state = this.lstore
   },
 })
 </script>
