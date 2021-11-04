21e704bc6f0cb7b7013dd1564790dbfdadd374ea
emingenc <emingenc@novit.ai>
2021-11-04 23:05:40 +0300

    ADD: select todo

---
+++ src/components/selectTodo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="q-pa-md" style="max-width: %100;">
+    <div class="q-gutter-md">
+      <q-select filled v-model="store.state.currentTodo" :options="store.state.todos" label="focus on just one task" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref,inject } from 'vue'
+
+export default {
+  setup () {
+    const store = inject('store')   
+    return {
+      store,  
+    }
+  }
+}
+</script>
+<style>
+
+</style>
