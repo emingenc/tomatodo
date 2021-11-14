b1430d06fc71612cd6a01e217ff5b535eafb00b5
emingenc <emingenc@novit.ai>
2021-11-14 20:40:24 +0300

    add admob

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 <script>
 import { defineComponent, provide } from 'vue';
 import store from './store'
+import { AdMobPlus, BannerAd } from '@admob-plus/capacitor'
 
 export default defineComponent({
   name: 'App',
@@ -12,6 +13,16 @@ export default defineComponent({
     provide('store',store)
   },
   mounted(){
+    async () => {
+  const banner = new BannerAd({
+    adUnitId: 'ca-app-pub-1161509012137925/9039660385',
+  })
+  await banner.show()
+
+  AdMobPlus.addListener('banner.impression', async () => {
+    await banner.hide()
+  })
+}
     
   },
 })
