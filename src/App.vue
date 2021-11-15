6940b0f74e440a8979af3e4df30b1ed59c6d2248
Emin GENC <81563574+emingenc@users.noreply.github.com>
2021-11-15 09:52:20 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -12,18 +12,6 @@ export default defineComponent({
     
     provide('store',store)
   },
-  mounted(){
-    async () => {
-  const banner = new BannerAd({
-    adUnitId: 'ca-app-pub-1161509012137925/9039660385',
-  })
-  await banner.show()
-
-  AdMobPlus.addListener('banner.impression', async () => {
-    await banner.hide()
-  })
-}
-    
-  },
+  mounted(){ },
 })
 </script>
