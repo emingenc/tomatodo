8b5475b9517032009f32940aab99460897044fda
Emin GENC <81563574+emingenc@users.noreply.github.com>
2021-11-15 09:54:23 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
 <script>
 import { defineComponent, provide } from 'vue';
 import store from './store'
-import { AdMobPlus, BannerAd } from '@admob-plus/capacitor'
 
 export default defineComponent({
   name: 'App',
